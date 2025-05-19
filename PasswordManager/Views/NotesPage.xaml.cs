ccfaf755acedbbce86169ce33129ff2e1688194e
Reyme87 <astafev.ivan.0505@mail.ru>
2025-05-19 15:13:48 +0300

    Добавление навигации между окнами

--- PasswordManager/Views/NotesPage.xaml.cs
+++ PasswordManager/Views/NotesPage.xaml.cs
@@ -24,5 +24,10 @@ namespace PasswordManager.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+        }
     }
 }
