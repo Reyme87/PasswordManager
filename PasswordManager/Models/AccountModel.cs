9e2f5e359007497612d3c602bfb868fcbe933293
Reyme87 <astafev.ivan.0505@mail.ru>
2025-05-19 22:40:19 +0300

    Создание моделей аккаунта и методов оплаты

---
+++ PasswordManager/Models/AccountModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PasswordManager.Models
+{
+    class AccountModel
+    {
+        private string? _username;
+        private string? _password;
+        private string? _url;
+
+        public AccountModel(string Username, string Password, string Url) 
+        {
+            _username = Username;
+            _password = Password;
+            _url = Url;
+        }
+
+        public string? Username
+        { 
+            get => _username; 
+            set => _username = value; 
+        }
+
+        public string? Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public string? Url
+        {
+            get => _url;
+            set => _url = value;
+        }
+    }
+}
---
+++ PasswordManager/Models/CardModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PasswordManager.Models
+{
+    class CardModel
+    {
+        private string? _number;
+        private string? _mmyy;
+        private int? _cvv;
+
+        public CardModel(string Number, string MmYy, int Cvv)
+        {
+            _number = Number;
+            _mmyy = MmYy;
+            _cvv = Cvv;
+        }
+
+        public string? Number 
+        { 
+            get => _number; 
+            set => _number = value; 
+        }
+
+        public string? MmYy 
+        { 
+            get => _mmyy; 
+            set => _mmyy = value; 
+        }
+
+        public int? Cvv
+        {
+            get => _cvv;
+            set => _cvv = value;
+        }
+    }
+}
