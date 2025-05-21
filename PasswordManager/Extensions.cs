3664fd48852f7aa9ffddc2f62387d2e430222164
Reyme87 <astafev.ivan.0505@mail.ru>
2025-05-21 23:29:45 +0300

    Создание класса расширений

---
+++ PasswordManager/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    internal static class Extensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            return new ObservableCollection<T>(source);
+        }
+    }
+}
