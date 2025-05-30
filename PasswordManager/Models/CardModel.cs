460c9644bd617a9a9375abca811728d541bedd62
Reyme87 <astafev.ivan.0505@mail.ru>
2025-05-30 21:40:45 +0300

    Изменение конструктора модели

--- PasswordManager/Models/CardModel.cs
+++ PasswordManager/Models/CardModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Policy;
 
 namespace PasswordManager.Models
 {
@@ -6,17 +7,19 @@ namespace PasswordManager.Models
     {
         private int[] _numberValues;
         private int[] _numberKeys;
-        private int? _mmyy;
+        private int _mmyy;
         private int[] _cvvValues;
         private int[] _cvvKeys;
+        private string _lastNumbers;
 
-        public CardModel(int[] NumberValues, int[] NumberKeys, int MmYy, int[] CvvValues, int[] CvvKeys)
+        public CardModel(int[] NumberValues, int[] NumberKeys, int MmYy, int[] CvvValues, int[] CvvKeys, string LastNumbers)
         {
             _numberValues = NumberValues;
             _numberKeys = NumberKeys;
             _mmyy = MmYy;
             _cvvValues = CvvValues;
             _cvvKeys = CvvKeys;
+            _lastNumbers = LastNumbers;
         }
 
         public int[] NumberValues
@@ -31,7 +34,7 @@ namespace PasswordManager.Models
             set => _numberKeys = value;
         }
 
-        public int? MMYY
+        public int MMYY
         {
             get => _mmyy;
             set => _mmyy = value;
@@ -47,6 +50,23 @@ namespace PasswordManager.Models
         {
             get => _cvvKeys;
             set => _cvvKeys = value;
+        }
+
+        public string LastNumbers
+        {
+            get => _lastNumbers;
+            set => _lastNumbers = value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is CardModel card) return (LastNumbers == card.LastNumbers && MMYY == card.MMYY);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return LastNumbers.GetHashCode();
         }
     }
 }
