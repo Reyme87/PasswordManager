765871159351640f2d76055f8b9640f97b06e474
Reyme87 <astafev.ivan.0505@mail.ru>
2025-06-02 00:21:22 +0300

    Создание анимации поисковой строки и уведомления о копировании

--- PasswordManager/Views/PasswordsPage.xaml.cs
+++ PasswordManager/Views/PasswordsPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -22,6 +23,7 @@ namespace PasswordManager.Views
     public partial class PasswordsPage : Page
     {
         private readonly PasswordsViewModel _vm = PasswordsViewModel.Instance;
+        private bool isClicked = false;
         public PasswordsPage()
         {
             InitializeComponent();
@@ -42,6 +44,39 @@ namespace PasswordManager.Views
         private void ChangeButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new AddAccountPage());
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            if (!isClicked)
+            {
+                anim.From = 35;
+                anim.To = 224;
+                anim.Completed += (s, e) => ResetButton.Visibility = Visibility.Visible;
+                isClicked = true;
+            }
+            else
+            {
+                anim.From = 224;
+                anim.To = 35;
+                ResetButton.Visibility = Visibility.Hidden;
+                isClicked = false;
+            }
+            anim.Duration = TimeSpan.FromSeconds(0.5);
+            anim.EasingFunction = new QuadraticEase();
+            SearchBox.BeginAnimation(WidthProperty, anim);
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.From = 0;
+            anim.To = 3;
+            anim.AutoReverse = true;
+            anim.Duration = TimeSpan.FromSeconds(1.5);
+            anim.EasingFunction = new QuadraticEase();
+            NotificationBorder.BeginAnimation(OpacityProperty, anim);
         }
     }
 }
