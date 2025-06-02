748b0849205896549cff3fc2f06f99ed51ba5262
Reyme87 <astafev.ivan.0505@mail.ru>
2025-06-03 01:05:19 +0300

    Создание уведомления о копировании

--- PasswordManager/Views/CardsPage.xaml.cs
+++ PasswordManager/Views/CardsPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -41,6 +42,17 @@ namespace PasswordManager.Views
         private void ChangeButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new AddCardPage());
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.From = 0;
+            anim.To = 3;
+            anim.AutoReverse = true;
+            anim.Duration = TimeSpan.FromSeconds(1.5);
+            anim.EasingFunction = new QuadraticEase();
+            NotificationBorder.BeginAnimation(OpacityProperty, anim);
         }
     }
 }
