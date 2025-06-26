46dabeae9a23e035fe57b85c3cd56247983cd226
Reyme87 <astafev.ivan.0505@mail.ru>
2025-06-26 21:26:08 +0300

    Изменение дизайна окна записей

--- PasswordManager/Views/NotesPage.xaml.cs
+++ PasswordManager/Views/NotesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace PasswordManager.Views
     /// </summary>
     public partial class NotesPage : Page
     {
+        private readonly NotesViewModel _vm = NotesViewModel.Instance;
         public NotesPage()
         {
             InitializeComponent();
+            DataContext = _vm;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
