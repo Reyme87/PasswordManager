1e0af8c753d77b3f1de8feb4223132641b551e7a
Reyme87 <astafev.ivan.0505@mail.ru>
2025-06-26 21:25:28 +0300

    Создание новой ViewModel для Notes

---
+++ PasswordManager/ViewModels/NotesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.ViewModels
+{
+    internal class NotesViewModel
+    {
+        private static NotesViewModel _instance;
+        public static NotesViewModel Instance => _instance ??= new NotesViewModel();
+
+
+    }
+}
