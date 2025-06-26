ccf5aaf29bc1f059289f09b667abd03174418ed9
Reyme87 <astafev.ivan.0505@mail.ru>
2025-06-26 21:25:46 +0300

    Создание страницы добавления записей

---
+++ PasswordManager/Views/AddNotesPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PasswordManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordManager.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNotesPage.xaml
+    /// </summary>
+    public partial class AddNotesPage : Page
+    {
+        private readonly NotesViewModel _vm = NotesViewModel.Instance;
+        public AddNotesPage()
+        {
+            InitializeComponent();
+            DataContext = _vm;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new NotesPage());
+        }
+    }
+}
