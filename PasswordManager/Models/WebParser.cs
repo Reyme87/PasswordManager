288d354836e14c8570c1b841f21d28966c3e92e7
Reyme87 <astafev.ivan.0505@mail.ru>
2025-09-12 22:33:28 +0300

    Создание класса WebParser для сбора логотипов сайтов

---
+++ PasswordManager/Models/WebParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Models
+{
+    class WebParser
+    {
+        private readonly HttpClient _httpClient;
+
+        public WebParser()
+        {
+            _httpClient = new HttpClient();
+
+        }
+
+        public async Task<bool> CheckExistanceAsync(string url)
+        {
+            try
+            {
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = "https://" + url;
+                }
+
+                using var request = new HttpRequestMessage(HttpMethod.Head, url);
+                using var response = await _httpClient.SendAsync(request);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<string> GetFaviconUriAsync(string url)
+        {
+            try
+            {
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = "https://" + url;
+                }
+
+                var baseUri = new Uri(url);
+                var faviconUrl = new Uri(baseUri, "/favicon.ico").AbsoluteUri;
+
+                var response = await _httpClient.GetAsync(faviconUrl);
+                if (response.IsSuccessStatusCode)
+                    return faviconUrl;
+                return "";
+            }
+            catch 
+            {
+                return "";
+            }
+        }
+    }
+}
